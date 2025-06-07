72dd066c190677b9551cdb636630b1750c1cda2a
Nabila Febriyanti Valentin <vanabila634@gmail.com>
2025-06-07 17:04:05 +0700

    solve_error

--- automate_Nabila_Febriyanti_Valentin.py
+++ automate_Nabila_Febriyanti_Valentin.py
@@ -31,3 +31,11 @@ def preprocess_data(filepath):
     )
 
     return X_train, X_test, y_train, y_test
+
+if __name__ == "__main__":
+    X_train, X_test, y_train, y_test = preprocess_data('healthcare-dataset-stroke-data.csv')
+
+    pd.DataFrame(X_train).to_csv('X_train.csv', index=False)
+    pd.DataFrame(X_test).to_csv('X_test.csv', index=False)
+    y_train.to_csv('y_train.csv', index=False)
+    y_test.to_csv('y_test.csv', index=False)
